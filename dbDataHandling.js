594b4c50a5394cd1b4b06df149f3b5d08b8bdca6
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-02-18 22:50:59 -0300

    dbDataHandling.js: first prototype of function that write data to DB

--- dbDataHandling.js
+++ dbDataHandling.js
@@ -86,18 +86,47 @@ function queryReadDb(db, queryStr, callback){
     })
 }
 
-function queryWriteDb(db, dataToWrite){
+function queryWriteDb(db, tableName, dataToWrite){
     /* Write data to the specified DB
-       It does all the job for now */
+       must receive an array with ordered data to be saved, according to the DB table convention */
+
+    var howManyProperties = "(?";
+    for(var i = 1; i < dataToWrite.length; i++){//the first ? is already there
+        howManyProperties += ", ?";
+    }
+    howManyProperties += ")"
+
+    db.transaction(function(tx){
+        // Insert entry - don't care about duplicates right now
+        try{
+            var res = tx.executeSql("INSERT INTO " + tableName + " VALUES" + howManyProperties,
+                          //[d.value, d.exptype, d.category, d.description, d.datestring])
+                                    dataToWrite)
+            console.log("dbDataHandling: queryWriteDb: Entry added")
+            console.log("\t" + dataToWrite)
+        }
+        catch(err){
+            console.error("dbDataHandling: queryWriteDb: error inserting into table exprev: " + err)
+        }
+
+        try{
+            var res = tx.executeSql("SELECT last_insert_rowid() FROM exprev")
+            console.log("dbDataHandling: queryWriteDb: last inserted rowid: " + JSON.stringify(res))
+            return Number(res.insertId) //using this is trouble once start deleting entries!
+        }
+        catch(err){
+            console.log("dbDataHandling: queryWriteDb: error getting last inserted rowid: " + err)
+        }
+    })
 }
 
 function query2string(queryInput, callback){
     /* Format the query result - all fields to strings
        The queryInput can be generated with queryReadDb() */
 
-    console.log("dbDataHandling.js: query2string: starting conversion")
-    console.log("dbDataHandling.js: query2string: input length: " + queryInput.rows.length)
+    //console.log("dbDataHandling.js: query2string: input length: " + queryInput.rows.length)
     var queryOutput = [];
+
     for(var i = 0; i < queryInput.rows.length; i++){
         var image;
         if(queryInput.rows.item(i).exptype === 0) image = "images/button_expense.png"
@@ -115,63 +144,8 @@ function query2string(queryInput, callback){
                             "rowid": queryInput.rows.item(i).rowid
                           })
     }
-    console.log("dbDataHandling.js: query2string: output length: " + queryOutput.length)
-}
 
-function string2query(queryInput){
-    /* Format strings from user input to be saved to db
-       The queryInput can be generated with queryDb() */
-}
-
-function loadDataFromDb(db, yearMonthString){
-    //console.log(JSON.stringify(listOfExpRevs))
-    //console.log(JSON.stringify(expRevListView.count))
-    var currentDate = new Date()
-    var cdateStr = currentDate.getDate().toString();
-    while (cdateStr.length < 2) { cdateStr = "0" + cdateStr}
-    db.transaction(function(tx){
-        try{
-            //var res = tx.executeSql("SELECT * FROM exprev WHERE strftime('%Y-%m-%d', datestring) BETWEEN '2017-12-01' AND '2017-12-31'")
-            var res = tx.executeSql("SELECT rowid, * FROM exprev " +
-                                    "WHERE strftime('%Y-%m', datestring) = '" + yearMonthString + "' " +
-                                    "ORDER BY datestring")
-            for(var i = 0; i < res.rows.length; i++){
-                var image;
-                if(res.rows.item(i).exptype === 0) image = "images/button_expense.png"
-                else if(res.rows.item(i).exptype === 1) image = "images/button_revenue.png"
-                else if(res.rows.item(i).exptype === 2) image ="images/button_investment.png"
-                else image ="images/button_loan.png"
-                listOfExpRevs.append({   "value": res.rows.item(i).value.toFixed(2),
-                                         //"exptype": res.rows.item(i).exptype.toString(),
-                                         "exptype": res.rows.item(i).exptype,
-                                         "image": image,
-                                         "category": res.rows.item(i).category,
-                                         "description": res.rows.item(i).description,
-                                         //"datestring": res.rows.item(i).datestring.slice(8,10) + "/" + res.rows.item(i).datestring.slice(5,7) + "/" + res.rows.item(i).datestring.slice(2,4),
-                                         "datestring": res.rows.item(i).datestring.slice(8,10),
-                                         "rowid": res.rows.item(i).rowid
-                                  })
-                console.log("Date: " + res.rows.item(i).datestring)
-            }
-            listOfExpRevs.append({   "value": "",
-                                     "exptype": 0,
-                                     "image": "images/button_expense.png",
-                                     "category": "",
-                                     "description": "",
-                                     "datestring": cdateStr,//force a date
-                                     "rowid": null
-                              })
-            //listOfExpRevs.sync() // no need here, but needed in worker
-            expRevListView.currentIndex = expRevListView.count - 1 //index is unset - jump to last index
-            expRevListView.positionViewAtEnd() // this line removes animation when going to last index
-            //console.log(JSON.stringify(listOfExpRevs))
-            //console.log(JSON.stringify(expRevListView.count))
-            console.log(JSON.stringify(expRevListView.currentIndex))
-            console.log(JSON.stringify(expRevListView.model.get(0)))
-        }
-        catch(err){
-            console.log("Error reading from table exprev: " + err)
-
-        }
-    })
+    //console.log("dbDataHandling.js: query2string: output length: " + queryOutput.length)
+    if(typeof callback === "undefined") return queryOutput; //if no callback was passed
+    callback(false, queryOutput); return 0;
 }
