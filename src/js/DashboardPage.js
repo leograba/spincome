7118b08cc9426f6e0c23465eae66674509eacf60
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-05-27 11:33:10 -0300

    Craeted function to append information to dashboard. Notice that it must be appended in the right order to work well

--- src/js/DashboardPage.js
+++ src/js/DashboardPage.js
@@ -16,6 +16,59 @@ function getDashModel(){
     return dashboardModelData
 }
 
+function appendInfo(type, ratio, val, mdl){
+    /* Append specific information to the dashboard, with predefined labels and colors
+        order matters when appending, if needed to modify, use set instead */
+    var d = {vlue: String(val), progress: ratio};
+    switch(type){
+    case 0:
+        d.name = qsTr("Receita")
+        d.colorCode = "green"
+        break
+    case 1:
+        d.name = qsTr("Despesa")
+        d.colorCode = "red"
+        break
+    case 2:
+        d.name = qsTr("Investimento")
+        d.colorCode = "blue"
+        break
+    case 3:
+        d.name = qsTr("Empréstimo")
+        d.colorCode = "orange"
+        break
+    case 4:
+        d.name = qsTr("Saldo Total")
+        if(val >= 0)
+            d.colorCode = "green"
+        else
+            d.colorCode = "red"
+        break
+    case 5:
+        d.name = qsTr("Investimento Total")
+        d.colorCode = "blue"
+        break
+    case 6:
+        d.name = qsTr("Retorno de Investimento")
+        if(val >= 0)
+            d.colorCode = "green"
+        else
+            d.colorCode = "red"
+        break
+    case 7:
+        d.name = qsTr("Despesa/Orçamento")
+        if(val >= 0)
+            d.colorCode = "green"
+        else
+            d.colorCode = "red"
+        break
+    default:
+        console.error("DashboardPage.js: appendInfo: type not defined")
+        return
+    }
+    mdl.append(d)
+}
+
 function refreshInfo(mdl, month){
     /* Get data from the current user DB and displays
         the consolidated information on the dashboard*/
@@ -32,52 +85,6 @@ function refreshInfo(mdl, month){
 
     // Get all available data
     //var amonth = DataBase.genSqliteQuery(0, DataBase.getUsername(), "", "")
-
-    mdl.append({  name: qsTr("Receita"),
-                   colorCode: "green",
-                   progress: 0.5,
-                   vlue: "5000.00"
-    })
-
-    mdl.append({  name: "Despesa",
-                   colorCode: "red",
-                   progress: 0.3,
-                   vlue: "3000.00"
-    })
-
-    mdl.append({  name: "Investimento",
-                   colorCode: "blue",
-                   progress: 0.7,
-                   vlue: "7000.00"
-    })
-
-    mdl.append({  name: "Empréstimo",
-                   colorCode: "orange",
-                   progress: 1.0,
-                   vlue: "10000.00"
-    })
-
-    mdl.append({  name: "Saldo Total",
-                   colorCode: "green",
-                   progress: 0.5,
-                   vlue: "-1000.00"
-    })
-
-    mdl.append({  name: "Investimento Total",
-                   colorCode: "blue",
-                   progress: 0.5,
-                   vlue: "7000.00"
-    })
-
-    mdl.append({  name: "Retorno de Investimento",
-                   colorCode: "green",
-                   progress: 0.8,
-                   vlue: "32.1%"
-    })
-
-    mdl.append({  name: "Despesa/Orçamento",
-                   colorCode: "green",
-                   progress: 0.64,
-                   vlue: "3000.00/4700.00"
-    })
+    appendInfo(0, 0.4, 7000.00, mdl)
+    appendInfo(4, 0.6, -10000.00, mdl)
 }
