136e6548ac42eb915a16af737bf61414a1e5735b
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-09-20 23:41:38 -0300

    Preliminary support for translation in the Qt app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QDebug>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,13 @@ int main(int argc, char *argv[])
 #endif
 
     QGuiApplication app(argc, argv);
+
+    QTranslator translator;
+
+    // look up e.g. :/translations/spincome_pt_BR.qm
+    if (translator.load(QLocale(QLocale::Portuguese, QLocale::Brazil), QLatin1String("spincome"), QLatin1String("_"), QLatin1String(":/translations")))
+        app.installTranslator(&translator);
+    else qDebug() << "main.cpp: Translation file not found. Region is: " << QLocale(QLocale::Portuguese, QLocale::Brazil).uiLanguages();
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/src/qml/main.qml")));
